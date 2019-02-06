a90058cefcda3769b868047f886733895aedf396
Jonas Schievink <jonasschievink@gmail.com>
2019-02-06 11:59:52 +0100

    empty lines dont show up in output

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+mod tests {
+    use insta::assert_snapshot_matches;
+
+    #[test]
+    fn bla() {
+        assert_snapshot_matches!(
+            "bla",
+            r#"
+First line
+
+Second line
+"#
+        );
+    }
+}
